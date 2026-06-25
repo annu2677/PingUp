d9ed3447830ceba05da1a7dd68941dc2f8e469fc
Annoaja <pannoaja40@gmail.com>
2026-06-25 23:18:01 +0530

    Nav button issues fixed

--- backend/src/main/java/com/annoaja/backend/controller/UserController.java
+++ backend/src/main/java/com/annoaja/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import com.annoaja.backend.service.UserService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+
 import java.util.Map;
 
 @RestController
@@ -19,6 +20,11 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User user) {
         try {
@@ -30,27 +36,23 @@ public class UserController {
         }
     }
 
-//    @PostMapping("/register")
-//    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-//    //public AuthResponse register(@RequestBody User user) {
-//        //return userService.registerUser(user);
-//        return ResponseEntity.status(HttpStatus.CONFLICT).body(Map.of("message", "Email already registered"));
-//    }
-
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody User user) {
         try {
-            AuthResponse response = userService.loginUser(user.getEmail(), user.getPassword());
+            AuthResponse response = userService.loginUser(
+                    user.getEmail(),
+                    user.getPassword()
+            );
             return ResponseEntity.ok(response);
-        }
-        catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(Map.of("message", e.getMessage()));
+        } catch (RuntimeException e) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", e.getMessage()));
         }
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getUser(@PathVariable String id) {
-
         try {
             return ResponseEntity.ok(userService.getUserById(id));
         }
@@ -66,11 +68,6 @@ public class UserController {
         }
         catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("message", e.getMessage()));
-        }
-
-        @GetMapping
-        public ResponseEntity<?> getAllUsers() {
-            return ResponseEntity.ok(userService.getAllUsers());
         }
     }
 }
